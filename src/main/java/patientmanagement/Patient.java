3de2c44c63ec99de42aa92de4ecdf8a0612636cb
Christopher King <Chris.King@keyin.com>
2026-03-06 11:36:40 -0330

    Second commit, added Patient class

--- src/main/java/patientmanagement/Patient.java
+++ src/main/java/patientmanagement/Patient.java
@@ -1,4 +1,32 @@
 package patientmanagement;
 
 public class Patient {
+    private int patientId;
+    private String name;
+    private String reasonForVisit;
+
+    public Patient(int patientId, String name, String reasonForVisit) {
+        this.patientId = patientId;
+        this.name = name;
+        this.reasonForVisit = reasonForVisit;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReasonForVisit() {
+        return reasonForVisit;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + patientId +
+                ", Name: " + name +
+                ", Reason for Visit: " + reasonForVisit;
+    }
 }
